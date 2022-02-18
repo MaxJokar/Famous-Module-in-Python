af16c4966d2638f9accf9281c1b82309fd7cb19f
email <mmb2020max@gmail.com>
2022-02-18 12:30:57 +0200

    template capwords pritable punt Done

--- StringModule.py
+++ StringModule.py
@@ -1,3 +1,4 @@
+from re import template
 import string
 import datetime
 
@@ -5,7 +6,7 @@ import datetime
 # print(dir(string))
 # print()
 
-print(dir(datetime))
+# print(dir(datetime))
 # print(vars(datetime)) #More datailed
 # print(datetime.__doc__ )
 # print(datetime.__file__ ) #the address of  datetime Module in our system 
@@ -21,3 +22,45 @@ print(dir(datetime))
 # help(datetime.datetime)
 
 # help(int)
+#==============================================================
+
+ 
+# print(string.ascii_letters):for number , Big Letter or small ones 
+
+# print(string.ascii_lowercase)
+# print(string.ascii_uppercase)
+# print(string.digits)
+# print(string.punctuation) # All  signs in Python
+# print(string.printable)  # all things printable 
+
+# str1=string.capwords("jack philiP londoN") # First Letter of a string converted to Capital and the other in to small
+# print(str1)
+
+
+
+template1=string.Template("Answer is : $x > $y")
+str1=template1.substitute(x=22 ,y=18)
+print(str1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
