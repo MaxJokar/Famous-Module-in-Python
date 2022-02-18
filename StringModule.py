3519f6144297ab1a7f37c2df564c3c1f0a45cc8d
email <mmb2020max@gmail.com>
2022-02-18 12:20:23 +0200

    doc file name cache help

--- StringModule.py
+++ StringModule.py
@@ -2,7 +2,22 @@ import string
 import datetime
 
 
-
-
+# print(dir(string))
+# print()
 
 print(dir(datetime))
+# print(vars(datetime)) #More datailed
+# print(datetime.__doc__ )
+# print(datetime.__file__ ) #the address of  datetime Module in our system 
+# print(datetime.__name__ ) 
+# print(datetime.__cached__ ) 
+
+
+
+#any info about  a  Module or  any class by:
+
+# help(print)
+# help(datetime.datetime.now)
+# help(datetime.datetime)
+
+# help(int)
